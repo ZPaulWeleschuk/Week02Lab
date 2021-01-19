2a6dd2ca516aa7b723763259addf3588d02464ec
843876 <843876@SAIT225726IT.ACDM.DS.SAIT.CA>
2021-01-18 23:10:44 -0700

    commit a hello world app for testing

---
+++ src/lab2helloworld/Lab2HelloWorld.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2helloworld;
+
+/**
+ *
+ * @author 843876
+ */
+public class Lab2HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(" ______________");
+        System.out.println("||            ||");
+        System.out.println("||            ||");
+        System.out.println("||Hello World ||");
+        System.out.println("||   Lab 02   ||");
+        System.out.println("||____________||");
+        System.out.println("|______________|");
+        System.out.println(" \\\\############\\\\");
+        System.out.println("  \\\\############\\\\");
+        System.out.println("   \\      ____    \\");
+        System.out.println("    \\_____\\___\\____\\");
+        //modified from source: https://www.asciiart.eu/computers/computers
+        
+    }
+    
+}
+        
